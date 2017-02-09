1c1fb53f9f026a7d537b8bf101497d9f63690a61
Olivier Bazoud <olivier.bazoud@gmail.com>
2017-02-09 22:29:17 +0100

    Add option to check topic's config

--- bin/check-topic.rb
+++ bin/check-topic.rb
@@ -56,6 +56,12 @@ class TopicsCheck < Sensu::Plugin::Check::CLI
          long: '--replication-factor REPLICATION_FACTOR',
          proc: proc(&:to_i)
 
+  option :configs,
+         description: 'Topic configurations',
+         short: '-c CONFIG',
+         long: '--configs CONFIG',
+         proc: proc { |a| JSON.parse(a) }
+
   def run
     z = Zookeeper.new(config[:zookeeper])
 
@@ -73,6 +79,14 @@ class TopicsCheck < Sensu::Plugin::Check::CLI
         min = partitions.min_by { |_, brokers| brokers.size }[1].length
         max = partitions.max_by { |_, brokers| brokers.size }[1].length
         critical "Topic '#{config[:name]}' RF is between #{min} and #{max}, expecting #{config[:replication_factor]}" if config[:replication_factor] != min || min != max
+      end
+
+      if config.key?(:configs)
+        config_data = z.get(path: "/config/topics/#{config[:name]}")[:data]
+        configs = JSON.parse(config_data)['config']
+        config[:configs].each do |k, v|
+          critical "Topic '#{config[:name]}': config #{k} is /#{v}, expecting #{configs[k]}" if !configs.key?(k) || configs[k].to_s != v.to_s
+        end
       end
     end
     ok
